fb2191b9e0d53137833065482727eaea88369461
rutus <rutus@phoenix.tech>
2026-03-27 14:26:45 -0500

    mockingapi

--- tests/workingWithApi.spec.ts
+++ tests/workingWithApi.spec.ts
@@ -7,6 +7,19 @@ test.beforeEach( async ({page}) =>{
             body:JSON.stringify(tags)
         })
     })
+
+    await page.route('*/**/api/articles*', async route =>{
+       const response = await route.fetch()
+       const responseBody = await response.json()
+       responseBody.articles[0].title = "This is a test title"
+       responseBody.articles[0].description = "This is a test description"
+
+       await route.fulfill({
+        body:JSON.stringify(responseBody)   
+       })
+
+    })
+
     await page.goto('https://conduit.bondaracademy.com/')
 })
 
@@ -14,4 +27,7 @@ test('First test to validate the title of the page', async ({page})=> {
     // page.pause();
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
     // page.pause()
+    await expect(page.locator('app-article-preview h1').first()).toContainText('This is a test title')
+    await expect(page.locator('app-article-preview p').first()).toContainText('This is a test description')
+
 })
